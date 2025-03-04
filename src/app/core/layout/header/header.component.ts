3c42ee3359c4320146ef504dc842182d9102be0c
Sergio González Sánchez <sergiogonzalez@itm.edu.co>
2025-03-04 13:35:49 -0500

    feat: update header component and app layout
    - Added `NbActionsModule`, `NbLayoutModule`, `NbUserModule`, and `NgOptimizedImage` imports to `HeaderComponent`
    - Updated `header.component.html` to include ITM Logo
    - Included `HeaderComponent` in `app.component.ts`

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {NbActionsModule, NbLayoutModule, NbUserModule} from "@nebular/theme";
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
     NbLayoutModule,
     NbActionsModule,
-    NbUserModule
+    NbUserModule,
+    NgOptimizedImage
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
